d3cd11710f623704b1cdcea8752132d1fbfb48b3
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2025-06-23 18:18:42 -0400

    update backend port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import apiRouter from "./routes";
 
 const app = express();
-const port = process.env.EXPRESS_PORT || 3000;
+const port = process.env.EXPRESS_PORT || 3001;
 
 app.use(express.json());
 
