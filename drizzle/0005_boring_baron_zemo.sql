64beda809ca62d9260ae17271592aeaf470d1962
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2025-07-06 12:59:27 -0400

    update setup instructions

--- drizzle/0005_boring_baron_zemo.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "data_sources" ADD CONSTRAINT "data_sources_name_unique" UNIQUE("name");--> statement-breakpoint
-ALTER TABLE "sports_leagues" ADD CONSTRAINT "sports_leagues_name_unique" UNIQUE("name");
